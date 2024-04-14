7b593924ffdeefb8ad3bce846dbd7b354419054e
CollinWilkerson <wilkerc2@my.erau.edu>
2024-04-13 19:53:36 -0700

    Coding Catapult

--- Knighty Night/Assets/Scripts/LaunchSystem/Crank.cs
+++ Knighty Night/Assets/Scripts/LaunchSystem/Crank.cs
@@ -9,6 +9,7 @@ public class Crank : MonoBehaviour
     [SerializeField] float _crankSpeed;
 
     private float power = 0f;
+    private bool powerUp = true;
 
     // Start is called before the first frame update
     void Awake()
@@ -19,12 +20,21 @@ public class Crank : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && !Fire)
         {
-            if(power > 0.1)
-            {
-                power += 0.1f;
-            }
+            if(power < 0.1f)
+                powerUp = true;
+            else if(power > 10)
+                powerUp = false;
+            if (powerUp)
+                power += 0.1f * Time.deltaTime;
+            else
+                power -= 0.1f * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Hinge Fire");
+            Fire = true;
         }
     }
 }
--- Knighty Night/Assets/Scripts/LaunchSystem/Knights.cs
+++ Knighty Night/Assets/Scripts/LaunchSystem/Knights.cs
@@ -8,16 +8,36 @@ public class Knights : MonoBehaviour
     //1 knight, 2 archer, 3 paladin
     [SerializeField] int _type = 1;
 
+    private Crank _crank;
+    private Catapult _spoon;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sprite;
+
+    //handles components attached to the gameobject
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
+
+        _rb.isKinematic = true;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _crank = GameObject.FindFirstObjectByType<Crank>();
+        _spoon = GameObject.FindFirstObjectByType<Catapult>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_crank.Fire)
+        {
+            Debug.Log("Knights Fire");
+            _rb.isKinematic = false;
+            _rb.AddForce(Vector2.up * 100);
+        }
     }
 }
